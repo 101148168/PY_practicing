5abce1d43d67830051822c7d2f088fa1a4572c8a
E.L <eric.lu.synology@gmail.com>
2016-12-15 11:42:47 +0100

    modified:   Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -1,34 +1,32 @@
-CIPHER = {'a':'f', 'b':'e', 'c':'d', 'd':'c', 'e':'b','f':'a', 'i':'z', 'o':'l'}
+CIPHER = {'a':'f', 'b':'e', 'c':'d', 'd':'c', 'e':'b','f':'a'}
 
 # Encode button
 def encode(msg):
-    print(msg,"encodes to: ",'%s' % ''.join(
+    print(msg,"encodes to: ",'%s' % '*E*L*'+''.join(
         CIPHER[ch] if ch in CIPHER.keys() else ch for ch in msg ))
+
 
 def revmapper(to):
     for k, v in CIPHER.items():
         if v == to: return k
         
-
 # Decode button
 def decode(msg):
     print(msg, "decodes to: ", '%s' % ''.join(
         revmapper(ch) if ch in CIPHER.values() else ch for ch in msg ))
+     
 
-def decide(choose, msg):
-    if choose in ['e', 'E']:
+def decide(msg):
+    jud = msg[0:5]  # *** or not
+    if jud != '*E*L*':
         encode(msg)
-    elif choose in [ 'd', 'D']:
-        decode(msg)
-    else :
-        print('Enter invalid please try again')
+    else:
+        decode(msg[5:])
         
 def main():
     while True:
         msg = input("Input words: ")
-        choose =input("Encode please enter 'E', Decode please enter 'D': ")
-        decide(choose, msg)
-
+        decide(msg)
 
 
 main()
