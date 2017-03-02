7ac858d10664da5105322419ac5f4695c68f3568
E.L <eric.lu.synology@gmail.com>
2017-03-01 22:31:57 -0500

    modified:   scrabble/scrabble.py

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -12,9 +12,6 @@ SCRABBLE_LETTER_VALUES = {
     '!': 0, 'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
 
 WORDLIST_FILENAME = "words.txt"
 
@@ -34,7 +31,6 @@ def load_words():
 def get_frequency_dict(sequence):
 
     
-    # freqs: dictionary (element_type -> int)
     freq = {}
     for x in sequence:
         freq[x] = freq.get(x,0) + 1
